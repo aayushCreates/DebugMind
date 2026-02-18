1ca35f8fceee560b79e2c246381f276774770680
AyushAJS <jangidayush89@gmail.com>
2026-02-18 19:15:15 +0530

    fix build

--- client/src/components/Auth.tsx
+++ client/src/components/Auth.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { X, Mail, Lock, User, Terminal } from "lucide-react";
 
 interface AuthModalType {
